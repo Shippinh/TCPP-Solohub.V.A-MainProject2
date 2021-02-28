881bfe81d5f671638af3bf7fe4f09675f1d051c3
Shippinh <78018399+Shippinh@users.noreply.github.com>
2021-02-28 18:48:46 +0200

    BROWSER

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Underworld!");//comment
+            Console.WriteLine("Hello Underworld!");//comment BROWSER
         }
     }
 }
